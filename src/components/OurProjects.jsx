d5be5d0327abc1d2efe6b33a3662b211758cb7f4
Alam Khan <alam.khan@urbanledger.app>
2022-08-07 19:15:24 +0530

    project section

--- src/components/OurProjects.jsx
+++ src/components/OurProjects.jsx
@@ -1,145 +1,271 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function OurProjects() {
-    return (
-        <div className="project-page-area area-padding-2">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12 col-sm-12 col-xs-12 ">
-                        <div className="section-headline text-center">
-                            <h3>Our successful goal</h3>
-                            <p>Dummy text is also used to demonstrate the appearance of different typefaces and layouts</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="project-content">
-                        <div className="col-md-4 col-sm-4 col-xs-12">
-                            <div className="single-awesome-project">
-                                <div className="awesome-img">
-                                    <a href="#">
-                                        <img src="assets/img/project/p1.jpg" alt="" />
-                                    </a>
-                                    <div className="add-actions text-center">   
-                                        <a className="venobox" data-gall="myGallery" href="img/project/p1.jpg">
-                                            <i className="port-icon ti-zoom-in" />
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="project-dec">
-                                    <Link to={'/project_details'}>
-                                        <h4>Data collection</h4>
-                                    </Link>
-                                    <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-sm-4 col-xs-12">
-                            <div className="single-awesome-project">
-                                <div className="awesome-img">
-                                    <a href="#">
-                                        <img src="assets/img/project/p2.jpg" alt="" />
-                                    </a>
-                                    <div className="add-actions text-center">
-                                        <a className="venobox" data-gall="myGallery" href="img/project/p2.jpg">
-                                            <i className="port-icon ti-zoom-in" />
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="project-dec">
-                                    <Link to={'/project_details'}>
-                                        <h4>Models development</h4>
-                                    </Link>
-                                    <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-sm-4 col-xs-12">
-                            <div className="single-awesome-project">
-                                <div className="awesome-img">
-                                    <a href="#">
-                                        <img src="assets/img/project/p3.jpg" alt="" />
-                                    </a>
-                                    <div className="add-actions text-center">
-                                        <a className="venobox" data-gall="myGallery" href="img/project/p3.jpg">
-                                            <i className="port-icon ti-zoom-in" />
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="project-dec">
-                                    <Link to={'/project_details'}>
-                                        <h4>Service deployment</h4>
-                                    </Link>
-                                    <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-sm-4 col-xs-12">
-                            <div className="single-awesome-project">
-                                <div className="awesome-img">
-                                    <a href="#">
-                                        <img src="assets/img/project/p4.jpg" alt="" />
-                                    </a>
-                                    <div className="add-actions text-center">
-                                        <a className="venobox" data-gall="myGallery" href="img/project/p4.jpg">
-                                            <i className="port-icon ti-zoom-in" />
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="project-dec">
-                                    <Link to={'/project_details'}>
-                                        <h4>Environment project</h4>
-                                    </Link>
-                                    <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-sm-4 col-xs-12">
-                            <div className="single-awesome-project">
-                                <div className="awesome-img">
-                                    <a href="#">
-                                        <img src="assets/img/project/p5.jpg" alt="" />
-                                    </a>
-                                    <div className="add-actions text-center">
-                                        <a className="venobox" data-gall="myGallery" href="img/project/p5.jpg">
-                                            <i className="port-icon ti-zoom-in" />
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="project-dec">
-                                    <Link to={'/project_details'}>
-                                        <h4>Design solution</h4>
-                                    </Link>
-                                    <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-sm-4 col-xs-12">
-                            <div className="single-awesome-project">
-                                <div className="awesome-img">
-                                    <a href="#">
-                                        <img src="assets/img/project/p6.jpg" alt="" />
-                                    </a>
-                                    <div className="add-actions text-center">
-                                        <a className="venobox" data-gall="myGallery" href="img/project/p6.jpg">
-                                            <i className="port-icon ti-zoom-in" />
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="project-dec">
-                                    <Link to={'/project_details'}>
-                                        <h4>Design solution</h4>
-                                    </Link>
-                                    <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="project-page-area area-padding-2">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 col-sm-12 col-xs-12 ">
+            <div className="section-headline text-center">
+              <h3>Our Successful Projects</h3>
+              <p>
+                Dummy text is also used to demonstrate the appearance of
+                different typefaces and layouts
+              </p>
             </div>
+          </div>
         </div>
-    )
+        <div className="row">
+          <div className="project-content">
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <div className="single-awesome-project">
+                <div className="awesome-img">
+                  <a href="#">
+                    <img src="assets/img/project/1tapcard.png" alt="" />
+                  </a>
+                  <div className="add-actions text-center">
+                    <a
+                      className="venobox"
+                      data-gall="myGallery"
+                      href="img/project/p1.jpg"
+                    >
+                      <i className="port-icon ti-zoom-in" />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-dec">
+                  <Link to={"/project_details"}>
+                    <h4>1 tap card</h4>
+                  </Link>
+                  <p>
+                    Our development opt in to the projects they genuinely want
+                    to work on, committing wholeheartedly to delivering.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <div className="single-awesome-project">
+                <div className="awesome-img">
+                  <a href="#">
+                    <img src="assets/img/project/aakhribaazi.png" alt="" />
+                  </a>
+                  <div className="add-actions text-center">
+                    <a
+                      className="venobox"
+                      data-gall="myGallery"
+                      href="img/project/p2.jpg"
+                    >
+                      <i className="port-icon ti-zoom-in" />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-dec">
+                  <Link to={"/project_details"}>
+                    <h4>Aakhri Baazi</h4>
+                  </Link>
+                  <p>
+                    Our development opt in to the projects they genuinely want
+                    to work on, committing wholeheartedly to delivering.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <div className="single-awesome-project">
+                <div className="awesome-img">
+                  <a href="#">
+                    <img src="assets/img/project/alnoortrading.png" alt="" />
+                  </a>
+                  <div className="add-actions text-center">
+                    <a
+                      className="venobox"
+                      data-gall="myGallery"
+                      href="img/project/p3.jpg"
+                    >
+                      <i className="port-icon ti-zoom-in" />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-dec">
+                  <Link to={"/project_details"}>
+                    <h4>Alnoor Trading</h4>
+                  </Link>
+                  <p>
+                    Our development opt in to the projects they genuinely want
+                    to work on, committing wholeheartedly to delivering.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <div className="single-awesome-project">
+                <div className="awesome-img">
+                  <a href="#">
+                    <img src="assets/img/project/apex.png" alt="" />
+                  </a>
+                  <div className="add-actions text-center">
+                    <a
+                      className="venobox"
+                      data-gall="myGallery"
+                      href="img/project/p4.jpg"
+                    >
+                      <i className="port-icon ti-zoom-in" />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-dec">
+                  <Link to={"/project_details"}>
+                    <h4>Apex</h4>
+                  </Link>
+                  <p>
+                    Our development opt in to the projects they genuinely want
+                    to work on, committing wholeheartedly to delivering.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <div className="single-awesome-project">
+                <div className="awesome-img">
+                  <a href="#">
+                    <img src="assets/img/project/diamondcrm.png" alt="" />
+                  </a>
+                  <div className="add-actions text-center">
+                    <a
+                      className="venobox"
+                      data-gall="myGallery"
+                      href="img/project/p5.jpg"
+                    >
+                      <i className="port-icon ti-zoom-in" />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-dec">
+                  <Link to={"/project_details"}>
+                    <h4>Diamond CRM</h4>
+                  </Link>
+                  <p>
+                    Our development opt in to the projects they genuinely want
+                    to work on, committing wholeheartedly to delivering.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <div className="single-awesome-project">
+                <div className="awesome-img">
+                  <a href="#">
+                    <img src="assets/img/project/intelmeta.png" alt="" />
+                  </a>
+                  <div className="add-actions text-center">
+                    <a
+                      className="venobox"
+                      data-gall="myGallery"
+                      href="img/project/p5.jpg"
+                    >
+                      <i className="port-icon ti-zoom-in" />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-dec">
+                  <Link to={"/project_details"}>
+                    <h4>Intelmeta Servicess</h4>
+                  </Link>
+                  <p>
+                    Our development opt in to the projects they genuinely want
+                    to work on, committing wholeheartedly to delivering.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <div className="single-awesome-project">
+                <div className="awesome-img">
+                  <a href="#">
+                    <img src="assets/img/project/mobilogi.png" alt="" />
+                  </a>
+                  <div className="add-actions text-center">
+                    <a
+                      className="venobox"
+                      data-gall="myGallery"
+                      href="img/project/p5.jpg"
+                    >
+                      <i className="port-icon ti-zoom-in" />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-dec">
+                  <Link to={"/project_details"}>
+                    <h4>Mobilogi</h4>
+                  </Link>
+                  <p>
+                    Our development opt in to the projects they genuinely want
+                    to work on, committing wholeheartedly to delivering.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <div className="single-awesome-project">
+                <div className="awesome-img">
+                  <a href="#">
+                    <img src="assets/img/project/prcrubber.png" alt="" />
+                  </a>
+                  <div className="add-actions text-center">
+                    <a
+                      className="venobox"
+                      data-gall="myGallery"
+                      href="img/project/p5.jpg"
+                    >
+                      <i className="port-icon ti-zoom-in" />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-dec">
+                  <Link to={"/project_details"}>
+                    <h4>Prcrubber</h4>
+                  </Link>
+                  <p>
+                    Our development opt in to the projects they genuinely want
+                    to work on, committing wholeheartedly to delivering.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-4 col-xs-12">
+              <div className="single-awesome-project">
+                <div className="awesome-img">
+                  <a href="#">
+                    <img src="assets/img/project/imaginem media.png" alt="" />
+                  </a>
+                  <div className="add-actions text-center">
+                    <a
+                      className="venobox"
+                      data-gall="myGallery"
+                      href="img/project/imaginem media.png"
+                    >
+                      <i className="port-icon ti-zoom-in" />
+                    </a>
+                  </div>
+                </div>
+                <div className="project-dec">
+                  <Link to={"/project_details"}>
+                    <h4>Imaginem Media</h4>
+                  </Link>
+                  <p>
+                    Our development opt in to the projects they genuinely want
+                    to work on, committing wholeheartedly to delivering.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default OurProjects
+export default OurProjects;
